43a637644b6a486426292dca8dd1e1eb561b7056
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2024-11-21 13:35:13 -0300

    Atualização do Main

--- AtualizaçãoPorMinuto/Main.py
+++ AtualizaçãoPorMinuto/Main.py
@@ -3,20 +3,29 @@ import pandas as pd
 from datetime import datetime
 import time
 
-requisicao = requests.get(
-    "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL, BTC-BRL")
+for i in range(2):
+    requisicao = requests.get(
+        "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 
-requisicao_dic = requisicao.json()
-cotDolar = requisicao_dic["USD-BRL"]["bid"]
-cotEuro = requisicao_dic["EUR-BRL"]["bid"]
-cotBTC = requisicao_dic["BTC-BRL"]["bid"]
+    requisicao_dic = requisicao.json()
 
-tabela = pd.read_excel("Cotações.xlsx")
-tabela.loc[0, 'Cotação'] = float(cotDolar)
-tabela.loc[1, 'Cotação'] = float(cotEuro)
-tabela.loc[2, 'Cotação'] = float(cotBTC) * 1000
-tabela.loc[0, "Data Última Atualização"] = datetime.now()
+    try:
+        cotDolar = requisicao_dic["USDBRL"]["bid"]
+        cotEuro = requisicao_dic["EURBRL"]["bid"]
+        cotBTC = requisicao_dic["BTCBRL"]["bid"]
+    except:
+        print("Erro ao acessar a API")
 
-tabela.to_excel("Cotações.xlsx", index=False)
+    tabela = pd.read_excel("Cotações.xlsx")
+    tabela.loc[0, 'Cotação'] = float(cotDolar)
+    tabela.loc[1, 'Cotação'] = float(cotEuro)
+    tabela.loc[2, 'Cotação'] = float(cotBTC) * 1000
+    tabela.loc[0, "Data Última Atualização"] = datetime.now()
 
-print(f"Cotação atualizada: {datetime.now()}")
+    try:
+        tabela.to_excel("Cotações.xlsx", index=False)
+    except:
+        print("Erro ao salvar arquivo")
+    print(f"Cotação atualizada: {datetime.now()}")
+
+    time.sleep(60)
